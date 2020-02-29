b06a4471f1268df793e2b47c1fe9b23b05012855
qespion <q.espion@gmail.com>
2020-02-29 17:38:19 +0100

    card && dicee

---
+++ i_am_poor/lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+      MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.white12,
+            title: Center(child: Text("I am poor")),
+          ),
+          backgroundColor: Colors.white60,
+        ),
+      ),
+    );
